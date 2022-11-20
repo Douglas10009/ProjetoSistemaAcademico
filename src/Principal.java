2a398b9e995c9566b1262202e47539e10ad1c483
Douglas10009 <douglasevertonev@gmail.com>
2022-11-19 23:27:17 -0300

    Organizações de detalhes, como tirar importações

--- src/Principal.java
+++ src/Principal.java
@@ -1,19 +1,15 @@
-import java.util.Scanner;
-
 // O aluno se inscreve na instituicao, dando o nome e CPF, e recebe o matricula e senha
 //Modelo de aviso: "-----> ..."
 
 public class Principal {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		//Antes de mostrar a tela de BemVInde, tem que verificar se o programa já foi rodado antes (/Banco de dados)
 		Menu.BemVinde();
 
-		
-
-		sc.close();
-
+		// Só pra eu me organizar --
+		// Menu.login();
+		// Menu.telaPrincipal();
 	}
 
 }
