18c2c239ceb2d6cff66a130dc8428a45faf0330b
Douglas10009 <douglasevertonev@gmail.com>
2022-11-25 21:56:30 -0300

    Organização do código

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 
 // O aluno se inscreve na instituicao, dando o nome e CPF, e recebe o matricula e senha
 //Modelo de aviso: "-----> ..."
@@ -12,17 +11,18 @@ public class Principal {
 	public static final String ANSI_ITALIC = "\u001B[3m";
 
 	public static void main(String[] args) {
-		//Antes de mostrar a tela de BemVInde, tem que verificar se o programa já foi rodado antes (/Banco de dados)
+		// Antes de mostrar a tela de BemVInde, tem que verificar se o programa já foi
+		// rodado antes (/Banco de dados)
 		while (true) {
 			Menu.Cadastrar();
-			
+
 		}
 
 		// Só pra eu me organizar --
 		// Menu.login();
 		// Menu.telaPrincipal();
 	}
-	
+
 	public static void clear() { // Método para limpar a tela
 		// Limpa a tela no windows, no linux e no MacOS
 		try {
@@ -30,13 +30,15 @@ public class Principal {
 				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 			else
 				Runtime.getRuntime().exec("clear");
-		} catch (Exception e) {}
+		} catch (Exception e) {
+		}
 	}
 
 	public static void sleep(int segundos) {
 		segundos *= 1000;// Transformar milisegundos em segundos
 		try {
 			Thread.sleep(segundos);
-		} catch (Exception e) {}
+		} catch (Exception e) {
+		}
 	}
 }
