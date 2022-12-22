90c224bfcb75a345acce699c6306aec351dd3b73
Douglas10009 <douglasevertonev@gmail.com>
2022-12-22 20:09:54 -0300

    Finalização do design do login

--- login.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class login {
-    public static void main(String[] args) {
-
-        
-    }
-}
--- src/ArquivoTeste.java
+++ src/ArquivoTeste.java
@@ -11,46 +11,7 @@ public class ArquivoTeste {
 
         // teste.forEach((k, v) -> System.out.printf("\n \nkey: %s | value: %s", k, v));
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("\n------ Sistema Acadêmico SA ------");
-        System.out.println("-----> Login ------");
-
-        System.out.print("\nDigite seu número de matrícula ");
-        String usuario = sc.next(); //Matricula
-
-        System.out.print("\nDigite sua senha: ");
-        String senha = sc.next(); //Cpf
-
-        if (Estudante.ListaEstudantes.containsKey(senha)) { //Verifica o CPF no HashMap estudante
-            //Verificar matricula do estudante
-            
-            String matricula_estudante = Estudante.ListaEstudantes.get(senha).getMatricula();
-           
-
-            if ( usuario.equals(matricula_estudante)) {
-                
-                System.out.println("Usuário logado");
-            } else {
-                System.out.println("Esse usuário/n° de matricula não está cadastrado");
-            }
-
-           
-        } else if (Docente.ListaDocentes.containsKey(senha)){ //Verifica o CPF no HashMap docente
-            //Verificar a matricula do docente
-
-            String matricula_docente = Docente.ListaDocentes.get(senha).getMatricula();
-            
-            if ( usuario.equals(matricula_docente)) {
-                
-                System.out.println("Usuário logado");
-            }else{
-                    System.out.println("Esse usuário/n° de matricula não está cadastrado");
-                }
-                
-        } else {
-            System.out.println("senha inválida!");
-        }
-    }
+        
     }
 }
+
