84409ba381f9cb17e33435f618ec95dbfeaef129
Douglas10009 <douglasevertonev@gmail.com>
2022-12-23 17:17:57 -0300

    turma

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -3,11 +3,12 @@ import java.util.Map;
 
 public class Disciplina {
     //Nome da disciplina - CPF docente
-    static Map<String, String> ListaDisciplina = new HashMap<String, String>();
+    static Map<String, String> ListaDisciplinaDocente = new HashMap<String, String>();
+    static Map<String, String> ListaDisciplinaTurma = new HashMap<String, String>();
 
     public static void CadastrarDisciplina(String nomeDisciplina , String cpf_Doscente){
         //Associar a disciplina a um docente
-        Disciplina.ListaDisciplina.put(nomeDisciplina, cpf_Doscente);
+        Disciplina.ListaDisciplinaDocente.put(nomeDisciplina, cpf_Doscente);
 
         //Como associar isso a uma turma?
     }
