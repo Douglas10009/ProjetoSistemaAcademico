41a2aaa1bb3a4e5f9c7012d5f24f0da0630db7b2
fabricia010108 <120727590+fabricia010108@users.noreply.github.com>
2022-12-23 12:40:12 +0000

    iniciando hashmap de notas

---
+++ notas02.java
@@ -0,0 +1,18 @@
+HashMap 
+import java.util.HashMap;
+import java.util.Scanner;
+public class notas02 {
+    
+
+
+    System.out.println("Digite as notas do alunos");
+    float nota1 = sc.nextFloat();
+    float nota2 = sc.nextFloat();
+    float nota3 = sc.nextFloat();
+
+    String Notas = nota1 + nota2 + nota3;
+
+Map <int ,Stringnotas> Notas = new HashMap<int,String>();
+     
+
+}
